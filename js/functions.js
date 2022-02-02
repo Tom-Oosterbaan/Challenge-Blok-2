5f19ff7ba9bea47ffbabb7e6299bfee2518e5cc0
Tom Oosterbaan <tom.oosterbaan@student.rocva.nl>
2022-02-02 21:17:34 +0100

    javascript toegevoegd

---
+++ js/functions.js
@@ -0,0 +1,9 @@
+function hideMe(){
+    document.getElementById("hide").style.display = "none"; 
+    document.getElementById("show").style.display = "block"; 
+}
+
+function show(){
+    document.getElementById("hide").style.display = "block";
+    document.getElementById("show").style.display = "none";     
+}
